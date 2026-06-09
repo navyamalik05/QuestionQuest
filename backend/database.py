6f4461e97270526d3de560ad54e62eabc1c56807
Navya Malik <malikna1@msu.edu>
2026-06-09 16:34:20 -0400

    Fix question media database fields

--- backend/database.py
+++ backend/database.py
@@ -1,14 +1,10 @@
+import os
+from datetime import datetime
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from datetime import datetime
-
-import os
-from dotenv import load_dotenv
-from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from datetime import datetime
 
 load_dotenv()
 
